d86ddaf20679d5725884c1841a9dbc483274a76a
Teodora <61601741+TeodoraSV@users.noreply.github.com>
2021-05-11 13:07:48 +0300

    Little changes.
    Adding colour to the help lines and tidying the code.

--- sketch.js
+++ sketch.js
@@ -1,8 +1,8 @@
 let pendulum;
 let slider;
 let slider1;
- var tx1
-  var tx2
+var tx1;
+var tx2;
 function preload(){
   ph=loadImage("ph.jpg")
 }
@@ -13,9 +13,9 @@ function setup() {
   slider=createSlider(9.51, 10.11,  9.81, 0.30)
   var l=createElement("p")
   var gr=slider.value()
-   slider1=createSlider(40, 400,  400, 120)
-    rr=slider1.value()
-   tx1=createElement("p", "g = "+slider.value())
+  slider1=createSlider(40, 400,  400, 120)
+  rr=slider1.value()
+  tx1=createElement("p", "g = "+slider.value())
   tx2=createElement("p", "L = "+slider1.value()/400 + " m")
   pendulum = new Pendulum(width / 2, 0, rr, gr);
   
@@ -27,18 +27,22 @@ function draw() {
 tx1.html("g = "+slider.value())
   tx2.html("L = "+slider1.value()/400+ " m")
   background(ph);
-  push()
+  
+ push()
   fill(0, 150)
   rect(0, 0, width, height)
-  pop()
+ pop()
+  
   pendulum.update();
   pendulum.drag(); // for user interaction
   pendulum.display();
-  push()
+  
+ push()
   translate(width/2, 0)
-   var l=line( pendulum.r * sin((15*PI)/180),  pendulum.r * cos((15*PI)/180), 0, 0);
-    var l1=line( pendulum.r * sin(-(15*PI)/180),  pendulum.r * cos(-(15*PI)/180), 0, 0);
-  pop()
+  stroke(150)
+  var l=line( pendulum.r * sin((15*PI)/180),  pendulum.r * cos((15*PI)/180), 0, 0);
+  var l1=line( pendulum.r * sin(-(15*PI)/180),  pendulum.r * cos(-(15*PI)/180), 0, 0);
+ pop()
 }
 
 function mousePressed() {
